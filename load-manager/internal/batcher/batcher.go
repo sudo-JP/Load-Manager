db78dadc9ef887fa3aa4c27b0a9be90268e103db
Jason Phan <jase.phan@mail.utoronto.ca>
2025-12-17 22:25:33 -0500

    user CRUD

---
+++ load-manager/internal/batcher/batcher.go
@@ -0,0 +1,35 @@
+package batcher
+
+import (
+	"sync"
+	"time"
+
+	"github.com/sudo-JP/Load-Manager/load-manager/internal/queue"
+)
+
+type PendingItem struct {
+	Data 		any
+	Timestamp 	time.Time
+}
+
+type Batcher struct {
+	queue *queue.Queue
+	
+	users 			[]*PendingItem
+	products 		[]*PendingItem
+	orders 			[]*PendingItem
+
+	batchSize 		int 
+	timeout 		time.Duration
+
+	mutex 			sync.Mutex
+
+	userTimer 		*time.Timer
+	productTimer 	*time.Timer
+	orderTimer 		*time.Timer
+}
+
+func (b *Batcher) AddUser(data any) {
+	
+}
+
--- load-manager/internal/routes/user.go
+++ load-manager/internal/routes/user.go
@@ -3,39 +3,151 @@ package routes
 import (
 	"encoding/json"
 	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sudo-JP/Load-Manager/load-manager/internal/batcher"
+	"github.com/sudo-JP/Load-Manager/load-manager/internal/queue"
 )
 
-func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(r.Body)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest) 
+type CreateUserDTO struct {
+    Name     string `json:"name" binding:"required"`
+    Email    string `json:"email" binding:"required,email"`
+    Password string `json:"password" binding:"required,min=8"`
+}
+
+func CreateUser(batch *batcher.Batcher) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var user CreateUserDTO
+
+		// Bind respond 
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		payload, _ := json.Marshal(user)
+
+		job := &queue.Job{
+			ID: 		queue.GetID(),
+			Resource: 	queue.User, 
+			CRUD: 		queue.Create,
+			Payload: 	payload, 
+			Priority: 	0, 
+			CreatedAt: 	time.Now(),
+		}
+
+		batch.AddUser(job)
+
+		c.Status(http.StatusOK)
 	}
 }
 
-func DeleteUsersHandler(w http.ResponseWriter, r *http.Request) {
 
+type GetUserDTO struct {
+	Email string `json:"email" binding:"required"`
+} 
+
+func GetUser(batch *batcher.Batcher) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		email := c.Query("email") 
+
+		if email == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "email required"})
+			return
+		}
+
+		payload, err := json.Marshal(DeleteUserDTO{Email: email})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "marshal failed"})
+			return
+		}
+
+		job := &queue.Job{
+			ID:        queue.GetID(),
+			Resource:  queue.User,
+			CRUD:      queue.Read,
+			Payload:   payload,
+			Priority:  0,
+			CreatedAt: time.Now(),
+		}
+
+		batch.AddUser(job)
+
+		c.Status(http.StatusOK)
+	}
 }
 
-func UpdateUsersHandler(w http.ResponseWriter, r *http.Request) {
 
+type UpdateUserDTO struct {
+    Name     string `json:"name" binding:"required"`
+    Email    string `json:"email" binding:"required,email"`
+    Password string `json:"password" binding:"required,min=8"`
 }
 
-func CreateUsersHandler(w http.ResponseWriter, r *http.Request) {
 
+func UpdateUser(batch *batcher.Batcher) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var user UpdateUserDTO 
+
+		// Bind respond 
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		payload, _ := json.Marshal(user)
+
+		job := &queue.Job{
+			ID: 		queue.GetID(),
+			Resource: 	queue.User, 
+			CRUD: 		queue.Update,
+			Payload: 	payload, 
+			Priority: 	0, 
+			CreatedAt: 	time.Now(),
+		}
+
+		batch.AddUser(job)
+
+		c.Status(http.StatusOK)
+	}
 }
 
-func UserRoutes(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		GetUsersHandler(w, r)
-	case http.MethodPost: 
-		CreateUsersHandler(w, r)
-	case http.MethodDelete: 
-		DeleteUsersHandler(w, r)
-	case http.MethodPut: 
-		UpdateUsersHandler(w, r)
-	default: 
-		http.Error(w, "bad method", http.StatusMethodNotAllowed)
+
+type DeleteUserDTO struct {
+	Email string `json:"email" binding:"required"`
+} 
+
+func DeleteUser(batch *batcher.Batcher) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		email := c.Query("email") 
+
+		if email == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "email required"})
+			return
+		}
+
+		payload, err := json.Marshal(DeleteUserDTO{Email: email})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "marshal failed"})
+			return
+		}
+
+		job := &queue.Job{
+			ID:        queue.GetID(),
+			Resource:  queue.User,
+			CRUD:      queue.Delete,
+			Payload:   payload,
+			Priority:  0,
+			CreatedAt: time.Now(),
+		}
+
+		c.Status(http.StatusOK)
+
+		batch.AddUser(job)
 	}
 }
