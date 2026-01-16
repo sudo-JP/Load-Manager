39c1d4271834866e6d008de0a88c95d143ab92c7
Jason Phan <jase.phan@mail.utoronto.ca>
2026-01-15 20:53:11 -0500

    random algorithm push

---
+++ load-manager/internal/queue/algorithms/random.go
@@ -0,0 +1,37 @@
+package algorithms
+
+import (
+	"sync"
+	"math/rand/v2"
+	"github.com/sudo-JP/Load-Manager/load-manager/internal/queue"
+)
+
+type Random struct {
+	jobs 		[]*queue.Job
+	mutex 		sync.Mutex
+}
+
+func (r *Random) Pushs(jobs []*queue.Job) []error {
+	r.mutex.Lock()	
+	defer r.mutex.Unlock()
+	r.jobs = append(r.jobs, jobs...)
+
+	errs := make([]error, 0)
+	return errs
+}
+
+func (r *Random) Pops() ([]*queue.Job, []error) {
+}
+
+func (r *Random) Len() int {
+	return len(r.jobs)
+}
+func (r *Random) IsEmpty() bool {
+	return r.Len() == 0
+}
+
+func NewRand() queue.Queue {
+	return &Random {
+		jobs: make([]*queue.Job, MIN_CAPACITY),
+	}
+}
